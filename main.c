51bc905edcb4d829fc3e6242a01cc9e74144d848
ducduy000 <167493730+ducduy000@users.noreply.github.com>
2024-09-22 14:53:06 +0700

    Update main.c

--- main.c
+++ main.c
@@ -491,16 +491,7 @@ void host_task(void *param)
 
 void app_main(){
 
-    //nvs_flash_init();                          // 1 - Initialize NVS flash using
-    // esp_nimble_hci_and_controller_init();      // 2 - Initialize ESP controller
-    //nimble_port_init();                        // 3 - Initialize the host stack
-    //ble_svc_gap_device_name_set("BLE-Server"); // 4 - Initialize NimBLE configuration - server name
-    //ble_svc_gap_init();                        // 4 - Initialize NimBLE configuration - gap service
-    //ble_svc_gatt_init();                       // 4 - Initialize NimBLE configuration - gatt service
-    //ble_gatts_count_cfg(gatt_svcs);            // 4 - Initialize NimBLE configuration - config gatt services
-    //ble_gatts_add_svcs(gatt_svcs);             // 4 - Initialize NimBLE configuration - queues gatt services.
-    //ble_hs_cfg.sync_cb = ble_app_on_sync;      // 5 - Initialize application
-    //nimble_port_freertos_init(host_task);      // 6 - Run the thread
+    
 
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
